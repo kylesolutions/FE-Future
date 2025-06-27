745055ef5afaf0be79f8089d7778d24630a94959
jayalakshmi kv <jlkshmikv@gmail.com>
2025-06-27 14:09:28 +0530

    url update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://143.110.178.225',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
